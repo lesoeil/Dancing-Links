8710058f1d77906ef66f626cbaabcb59cb832aa5
lesoeil <>
2018-06-05 20:52:09 +0800

    finish calculate 11 * 110 = 10010

--- FaultTesting-7-2-2-2.cpp
+++ FaultTesting-7-2-2-2.cpp
@@ -13,6 +13,13 @@ using namespace std;
 class MULT
 {
 public:
+	int get_z_1();
+	int get_z_2();
+	int get_z_3();
+	int get_z_4();
+	int get_z_5();
+
+public:
 	int set_x_1(int x1);	// input
 	int set_x_1_1(bool fault, int value);	// = x_1
 	int set_x_2_1(bool fault, int value);	// = x_1
@@ -26,7 +33,7 @@ public:
 	int set_y_1(int y1);	// input
 	int set_y_1_1(bool fault, int value);	// = y_1
 	int set_y_2_1(bool fault, int value);	// = y_1
-	int set_y_2(bool fault, int value);	// input
+	int set_y_2(int y2);	// input
 	int set_y_1_2(bool fault, int value);	// = y_2
 	int set_y_2_2(bool fault, int value);	// = y_2
 	int set_y_3(int y3);	// input
@@ -55,13 +62,13 @@ public:
 	int set_s(bool fault, int value);		// = a_1_3 ⊕ b_1_2
 	int set_s_1(bool fault, int value);	// = s
 	int set_s_2(bool fault, int value);	// = s
-	int set_p(bool fault, int value);		// = a_2_3 ⊕ b_2_2
+	int set_p(bool fault, int value);		// = a_2_3 ⋀ b_2_2
 	int set_z_3(bool fault, int value);	// = s_1 ⊕ c_1_1
 	int set_q(bool fault, int value);		// = s_2 ⋀ c_2_1
 	int set_c_2(bool fault, int value);	// = p ∨ q
 	int set_c_1_2(bool fault, int value);	// = c_2
 	int set_c_2_2(bool fault, int value);	// = c_2
-	int set_z_4(bool fault, int value);	// = b_1_3 ⊕ b_1_2
+	int set_z_4(bool fault, int value);	// = b_1_3 ⊕ c_1_2
 	int set_z_5(bool fault, int value);	// = b_2_3 ⋀ c_2_2
 
 private:
@@ -115,6 +122,31 @@ private:
 	int c_2_2;	// = c_2
 	int z_4;	// = b_1_3 ⊕ b_1_2
 	int z_5;	// = b_2_3 ⋀ c_2_2
+};
+
+int MULT::get_z_1()
+{
+	return z_1;
+}
+
+int MULT::get_z_2()
+{
+	return z_2;
+}
+
+int MULT::get_z_3()
+{
+	return z_3;
+}
+
+int MULT::get_z_4()
+{
+	return z_4;
+}
+
+int MULT::get_z_5()
+{
+	return z_5;
 }
 
 
@@ -184,6 +216,8 @@ int MULT::set_x_4_1(bool fault, int value)	// = x_1_1
 int MULT::set_x_2(int x2)	// input
 {
 	x_2 = x2;
+
+	return 0;
 }
 
 int MULT::set_x_1_2(bool fault, int value)	// = x_2
@@ -529,7 +563,7 @@ int MULT::set_b_2_2(bool fault, int value)	// = b_2
 	return 0;
 }
 
-int set_b_3(bool fault, int value)	// = x_4_2 ⋀ y_2_3
+int MULT::set_b_3(bool fault, int value)	// = x_4_2 ⋀ y_2_3
 {
 	if (fault)
 	{
@@ -669,7 +703,7 @@ int MULT::set_s_2(bool fault, int value)	// = s
 	return 0;
 }
 
-int MULT::set_p(bool fault, int value)		// = a_2_3 ⊕ b_2_2
+int MULT::set_p(bool fault, int value)		// = a_2_3 ⋀ b_2_2
 {
 	if (fault)
 	{
@@ -677,7 +711,7 @@ int MULT::set_p(bool fault, int value)		// = a_2_3 ⊕ b_2_2
 	}
 	else
 	{
-		p = a_2_3 ^ b_2_2;
+		p = a_2_3 & b_2_2;
 	}
 
 	return 0;
@@ -753,7 +787,7 @@ int MULT::set_c_2_2(bool fault, int value)	// = c_2
 	return 0;
 }
 
-int MULT::set_z_4(bool fault, int value)	// = b_1_3 ⊕ b_1_2
+int MULT::set_z_4(bool fault, int value)	// = b_1_3 ⊕ c_1_2
 {
 	if (fault)
 	{
@@ -761,7 +795,7 @@ int MULT::set_z_4(bool fault, int value)	// = b_1_3 ⊕ b_1_2
 	}
 	else
 	{
-		z_4 = b_1_3 ^ b_1_2;
+		z_4 = b_1_3 ^ c_1_2;
 	}
 
 	return 0;
@@ -781,10 +815,192 @@ int MULT::set_z_5(bool fault, int value)	// = b_2_3 ⋀ c_2_2
 	return 0;
 }
 
+
+int simulate()
+{
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	int y3;
+	bool fa[50];
+	int va[50];
+	MULT* m = new MULT;
+
+	for (int j=0; j<50; j++)
+	{
+		fa[j] = false;
+		va[j] = 1;
+	}
+
+	x1 = 1;
+	x2 = 1;
+	y1 = 0;
+	y2 = 1;
+	y3 = 1;
+
+	int i;
+
+	i=0;
+	m->set_x_1(x1);	// input
+
+	i=1;
+	m->set_x_1_1(fa[i], va[i]);	// = x_1
+	
+	i=2;
+	m->set_x_2_1(fa[i], va[i]);	// = x_1
+	
+	i=3;
+	m->set_x_3_1(fa[i], va[i]);	// = x_1_1
+	
+	i=4;
+	m->set_x_4_1(fa[i], va[i]);	// = x_1_1
+	
+	i=5;
+	m->set_x_2(x2);	// input
+	
+	i=6;
+	m->set_x_1_2(fa[i], va[i]);	// = x_2
+	
+	i=7;
+	m->set_x_2_2(fa[i], va[i]);	// = x_2
+
+	i=8;
+	m->set_x_3_2(fa[i], va[i]);	// = x_1_2
+
+	i=9;
+	m->set_x_4_2(fa[i], va[i]);	// = x_1_2
+
+	i=10;
+	m->set_y_1(y1);	// input
+
+	i=11;
+	m->set_y_1_1(fa[i], va[i]);	// = y_1
+
+	i=12;
+	m->set_y_2_1(fa[i], va[i]);	// = y_1
+
+	i=13;
+	m->set_y_2(y2);	// input
+
+	i=14;
+	m->set_y_1_2(fa[i], va[i]);	// = y_2
+
+	i=15;
+	m->set_y_2_2(fa[i], va[i]);	// = y_2
+
+	i=16;
+	m->set_y_3(y3);	// input
+
+	i=17;
+	m->set_y_1_3(fa[i], va[i]);	// = y_3
+
+	i=18;
+	m->set_y_2_3(fa[i], va[i]);	// = y_3
+
+	i=19;
+	m->set_z_1(fa[i], va[i]);	// = x_2_1 ⋀ y_1_1
+
+	i=20;
+	m->set_a_2(fa[i], va[i]);	// = x_3_1 ⋀ y_1_2
+
+	i=21;
+	m->set_a_1_2(fa[i], va[i]);	// = a_2
+
+	i=22;
+	m->set_a_2_2(fa[i], va[i]);	// = a_2
+
+	i=23;
+	m->set_a_3(fa[i], va[i]);	// = x_4_1 ⋀ y_1_3
+
+	i=24;
+	m->set_a_1_3(fa[i], va[i]);	// = a_3
+
+	i=25;
+	m->set_a_2_3(fa[i], va[i]);	// = a_3
+
+	i=26;
+	m->set_b_1(fa[i], va[i]);	// = x_2_2 ⋀ y_2_1
+
+	i=27;
+	m->set_b_1_1(fa[i], va[i]);	// = b_1
+
+	i=28;
+	m->set_b_2_1(fa[i], va[i]);	// = b_1
+
+	i=29;
+	m->set_b_2(fa[i], va[i]);	// = x_3_2 ⋀ y_2_2
+
+	i=30;
+	m->set_b_1_2(fa[i], va[i]);	// = b_2
+
+	i=31;
+	m->set_b_2_2(fa[i], va[i]);	// = b_2
+
+	i=32;
+	m->set_b_3(fa[i], va[i]);	// = x_4_2 ⋀ y_2_3
+
+	i=33;
+	m->set_b_1_3(fa[i], va[i]);	// = b_3
+
+	i=34;
+	m->set_b_2_3(fa[i], va[i]);	// = b_3
+
+	i=35;
+	m->set_z_2(fa[i], va[i]);	// = a_1_2 ⊕ b_1_1
+
+	i=36;
+	m->set_c_1(fa[i], va[i]);	// = a_2_2 ⋀ b_2_1
+
+	i=37;
+	m->set_c_1_1(fa[i], va[i]);	// = c_1
+
+	i=38;
+	m->set_c_2_1(fa[i], va[i]);	// = c_1
+
+	i=39;
+	m->set_s(fa[i], va[i]);		// = a_1_3 ⊕ b_1_2
+
+	i=40;
+	m->set_s_1(fa[i], va[i]);	// = s
+
+	i=41;
+	m->set_s_2(fa[i], va[i]);	// = s
+
+	i=42;
+	m->set_p(fa[i], va[i]);		// = a_2_3 ⋀ b_2_2
+
+	i=43;
+	m->set_z_3(fa[i], va[i]);	// = s_1 ⊕ c_1_1
+
+	i=44;
+	m->set_q(fa[i], va[i]);		// = s_2 ⋀ c_2_1
+
+	i=45;
+	m->set_c_2(fa[i], va[i]);	// = p ∨ q
+
+	i=46;
+	m->set_c_1_2(fa[i], va[i]);	// = c_2
+
+	i=47;
+	m->set_c_2_2(fa[i], va[i]);	// = c_2
+
+	i=48;
+	m->set_z_4(fa[i], va[i]);	// = b_1_3 ⊕ c_1_2
+
+	i=49;
+	m->set_z_5(fa[i], va[i]);	// = b_2_3 ⋀ c_2_2
+
+
+	cout<<m->get_z_5()<<m->get_z_4()<<m->get_z_3()<<m->get_z_2()<<m->get_z_1()<<endl;
+
+	return 0;
+}
+
+
 int main()
 {
-	// x_2 x_1 = 11, y_3 y_2 y_1 = 110
-	int x_1 = 1;
+	simulate();
 
 
 
