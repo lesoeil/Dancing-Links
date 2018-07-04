5b5b5760282697d0b91a26bf6360d8a687a8e9f0
lesoeil <>
2018-07-04 21:31:53 +0800

    improve generateWaerden by allow input of parameters j k n with command,

--- generateWaerden.cpp
+++ generateWaerden.cpp
@@ -10,6 +10,48 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	if (argc != 4)
+	{
+		cout<<"Command Usage: "<<argv[0]<<" j k n"<<endl;
+		cout<<"j k n are positive integers."<<endl;
+		cout<<"This command is to find binary sequence x_1 ... x_n that has no j \
+equally spaced 0s and k equally spaced 1s."<<endl;
+
+		return -1;
+	}
+
+	int j = std::stoi(argv[1]);
+	int k = std::stoi(argv[2]);
+	int n = std::stoi(argv[3]);; //total number of bits
+
+
+	for (int gap=1; gap<=((n-1)/(j-1)); gap++)
+	{
+		for (int base=1; base<= (n-((j-1)*gap)); base++)
+		{
+			for (int index=0; index<j; index++)
+			{
+				cout<<2*(base+index*gap)+1<<" ";
+			}
+			cout<<endl;
+		}
+	}
+
+
+	for (int gap=1; gap<=((n-1)/(k-1)); gap++)
+	{
+		for (int base=1; base<= (n-((k-1)*gap)); base++)
+		{
+			for (int index=0; index<k; index++)
+			{
+				cout<<2*(base+index*gap)<<" ";
+			}
+			cout<<endl;
+		}
+	}
+
+
+/*	
 	int n = 9; //total number of bits
 	int r = 3; 
 
@@ -31,7 +73,7 @@ int main(int argc, char** argv)
 			
 		}
 	}
-
+*/
 
 	/*
 	cout<<"1<<10 = "<<(1<<10)<<endl;
