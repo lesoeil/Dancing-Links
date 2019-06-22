2fcb1325f3902b89ce7632fb5e5940616ad5be38
lesoeil <>
2019-06-22 10:50:25 +0800

    compile pass

--- taocp_7_1_2_algoL.go
+++ taocp_7_1_2_algoL.go
@@ -16,14 +16,14 @@ func main() {
 		}
 	*/
 	var n uint
-	n = 5
+	n = 4
 	AlgoL(n)
 
 }
 
 /*AlgoL is the implementation of 7.1.2 Algorithm L (Find normal lengths).
 This algorithm determines L(f) for all normal truth tables 0 ≤ f < 2^(2^n-1),
-by building lists of all
+by building lists of all nonzero normal functions of length r for r ≥ 0.
 */
 func AlgoL(n uint) {
 
@@ -37,17 +37,27 @@ func AlgoL(n uint) {
 	}
 	L[0] = 0
 
-	type set map[uint64]bool
-	var list []set
+	type set64 map[uint64]bool
+
+	//To check how many list will be needed.
+	//for n = 4, seems the maximum length is 7 or 8?
+	var list [10]set64
+	for i := range list {
+		list[i] = make(set64)
+	}
 	x := make([]uint64, n+1)
 	for k := 1; k <= int(n); k++ {
 		x[k] = uint64((math.Pow(2, (math.Pow(2, float64(n)))) - 1) / (math.Pow(2, (math.Pow(2, float64(int(n)-k)))+1)))
 		fmt.Printf("%x ", x[k])
-		list[0][x[k]] = true
+		var y = list[0]
+		y[x[k]] = true
+
 	}
 
-	c := int(math.Pow(2, (math.Pow(2, float64(n)) - 1)))
-	fmt.Printf("0x%x Number of places when L(f) = ∞", c)
+	/*
+		c := int(math.Pow(2, (math.Pow(2, float64(n)) - 1)))
+		fmt.Printf("0x%x Number of places when L(f) = ∞", c)
+	*/
 
 }
 
