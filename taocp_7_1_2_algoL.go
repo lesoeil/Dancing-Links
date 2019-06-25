4b523ab334f97c446e70803de8edbd25becb8a80
lesoeil <>
2019-06-25 22:32:04 +0800

    continue to work with step L2, L3, L4, ...

--- taocp_7_1_2_algoL.go
+++ taocp_7_1_2_algoL.go
@@ -5,6 +5,14 @@ import (
 	"math"
 )
 
+//L is list
+var L []uint64
+
+type set64 map[uint64]bool
+
+// List is
+var list [10]set64
+
 /*AlgoL is the implementation of 7.1.2 Algorithm L (Find normal lengths).
 This algorithm determines L(f) for all normal truth tables 0 ≤ f < 2^(2^n-1),
 by building lists of all nonzero normal functions of length r for r ≥ 0.
@@ -15,18 +23,16 @@ func AlgoL(n uint64) {
 	// L1. Initialize.
 	var fTotal = Ppow2(n) / 2
 	fmt.Printf("n: %d ppower: %d\n", n, fTotal)
-	L := make([]uint64, fTotal)
+	L = make([]uint64, fTotal)
 
 	for i := range L {
 		L[i] = math.MaxUint64
 	}
 	L[0] = 0
 
-	type set64 map[uint64]bool
-
 	//To check how many list will be needed.
 	//for n = 4, seems the maximum length is 7 or 8?
-	var list [10]set64
+	//var list [10]set64
 	for i := range list {
 		list[i] = make(set64)
 	}
@@ -65,6 +71,32 @@ func AlgoL(n uint64) {
 	fmt.Printf("L(i) equal to %08x : %08x\n", uint64(math.MaxUint64), cc)
 	fmt.Printf("Number of places where L(f) = ∞ : %#x i.e. %d in decimal\n", c, c)
 
+	//L2. [Loop on r.] Do step L3 for r = 1, 2, ...; eventually the algorithm will
+	//		terminate when c becomes 0.
+	for r := 1; ; r++ {
+		if c == 0 {
+			fmt.Printf("c is %x , so terminate L2\n", c)
+			break
+		} else {
+			fmt.Printf("c is %x , so loop L2 still run.\n", c)
+		}
+		algoL3(r)
+	}
+
+}
+
+func algoL3(r int) {
+	fmt.Printf("Entering L3 with r = %x\n", r)
+	for j, k := 0, r-1; j <= k; j, k = j+1, k-1 {
+		algoL4(j, k)
+	}
+	fmt.Printf("Exit L3 with r = %x\n", r)
+}
+
+func algoL4(j int, k int) {
+	fmt.Printf("Entering L4 with j = %x  k = %x\n", j, k)
+
+	fmt.Printf("Exit L4 with j = %x  k = %x\n", j, k)
 }
 
 //Ppow2 return base 2 power of n
